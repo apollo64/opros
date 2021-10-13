06f6b50ab2d7b51eefcaf9911806aa1bc02633bc
apollo64 <apollo64@gmail.com>
2021-10-14 00:34:47 +0600

    models defined

--- survey/opros/admin.py
+++ survey/opros/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from survey.opros.models import ChoiceQuestion, Survey, Question, Participant
+
+admin.site.register(Participant)
+admin.site.register(Survey)
+admin.site.register(Question)
+admin.site.register(ChoiceQuestion)
+
--- survey/opros/models.py
+++ survey/opros/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Participant(User):
+    pass
+
+
+class Survey(models.Model):
+    participant = models.ForeignKey(Participant, on_delete=models.PROTECT, related_name="survey")
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=400,blank=True, null=True)
+    start_at = models.DateTimeField()
+    finish_at = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name}, {self.started_at}"
+
+class Question(models.Model):
+    survey =models.ForeignKey(Survey, on_delete=models.CASCADE, related_name="question")
+    text = models.TextField(max_length= 200)
+
+
+class ChoiceQuestion(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='choice')
+    text = models.CharField(max_length=100)
+    clicks = models.BooleanField(default=False)
+    def __str__(self):
+        return self.text
+
+
