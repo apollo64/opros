f861136a1e534fb095e8c8c37cb24b17cb13be5f
apollo64 <apollo64@gmail.com>
2021-11-10 12:52:08 +0600

    admin revised and edited

--- survey/opros/admin.py
+++ survey/opros/admin.py
@@ -3,8 +3,34 @@ from django.contrib import admin
 # Register your models here.
 from .models import Option, Survey, Question, PartiSession
 
+
+
+class OptionInline(admin.TabularInline):
+    model = Option
+    extra = 1
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['text']}),
+        ('Date information', {'fields': ['type', 'survey']}),
+    ]
+    readonly_fields = ['survey', 'type']
+    inlines = [OptionInline]
+
+class QuesInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class SurveyAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+        ('Type', {'fields': [ 'start_at',  'finish_at', 'description'] }),
+    ]
+    readonly_fields = ['start_at']
+    inlines = [QuesInline]
+
 admin.site.register(PartiSession)
-admin.site.register(Survey)
-admin.site.register(Question)
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Option)
 
